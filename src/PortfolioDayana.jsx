1f5766dbfdd77014fb5455c75bb7a9480b026782
nunsongi <nunsongi0613@gmail.com>
2025-11-10 15:52:44 -0500

    feat: updates

--- src/PortfolioDayana.jsx
+++ src/PortfolioDayana.jsx
@@ -155,7 +155,9 @@ export default function PortfolioDayana() {
                 <Tag tone={tone.chip}>Python</Tag>
                 <Tag tone={tone.chip}>Pandas</Tag>
                 <Tag tone={tone.chip}>Numpy</Tag>
-                <Tag tone={tone.chip}>Tailwind</Tag>
+                <Tag tone={tone.chip}>Matplotlib</Tag>
+                <Tag tone={tone.chip}>ScikitLearn</Tag>
+                <Tag tone={tone.chip}>ScikitLearn</Tag>
               </div>
             </div>
             <div className={`rounded-3xl border ${tone.card} p-6`}> 
